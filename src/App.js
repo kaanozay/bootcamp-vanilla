9f18e3c460220cb88ec869f76f8baf4601005129
kaanozay <kaan@sociality.io>
2021-09-11 16:33:18 +0300

    adding and deleting nodes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import TableComponent from "./components/TableComponent";
+//import TableComponent from "./components/TableComponent";
+
+import React, { useState } from 'react'
 
 const App = () => {
+
+
+  const [note, setNote] =useState("");
+
+  const [notes, setNotes] = useState([]);
+
+  const adding = () => {
+
+    setNotes([...notes, note]);  
+
+  }
+
+  const deleting = (id) => {
+
+   setNotes(notes.filter((item) => item.id !== id));
+    
+  }
+
+  const handleChange = (e) => {setNote(e.target.value)}
+
   return (
     <div className="App">
-      <TableComponent/>
+      
+      <textarea
+        name="note"
+        onChange={handleChange}
+        value={note}
+      />
+      <div>
+        <button onClick={() => adding()}>Create</button>
+      </div>
+      
+     
+     <div>
+     {notes.map((item) => {
+                return (
+                    <>
+                    <div>
+                        <h2>{item}</h2>
+                        <button onClick={() => deleting(item.id)}>Delete</button>
+                    </div>
+                        
+                    </>
+                );
+            })}
+     </div>
     </div>
   );
+
+
 }
 
 
